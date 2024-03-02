2e30c568b5cf1eb9382a96ec8711fc4108cd1bef
ngchisern <ncsern2000@gmail.com>
2024-03-03 01:50:43 +0800

    Add sample code for running the parser

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-// simple hello world 
-let message: string = "Hello World";
-console.log(message);
+import { CharStream, CommonTokenStream } from 'antlr4';
+import GoParser from './parser/GoParser'
+import GoLexer from './parser/GoLexer'
+
+function parseGoCode(code: string) {
+    const charStream = new CharStream(code);
+
+    const lexer = new GoLexer(charStream);
+
+    const tokenStream = new CommonTokenStream(lexer);
+
+    const parser = new GoParser(tokenStream);
+
+    let tree = parser.sourceFile();
+
+    console.log(tree);
+}
+
+const code = `
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+`;
+
+parseGoCode(code);
+
