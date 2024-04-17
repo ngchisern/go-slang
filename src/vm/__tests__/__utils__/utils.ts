b6b8a38e10f24cd20c75bbde1b650ca0aa647123
xyliew25 <e0550381@u.nus.edu>
2024-04-17 09:42:44 +0800

    Check logs in vm tests

--- src/vm/__tests__/__utils__/utils.ts
+++ src/vm/__tests__/__utils__/utils.ts
@@ -5,12 +5,18 @@ import { run } from '../../run'
 export interface Test {
   name: string
   input: string
+  output?: Set<any>
+  error?: string
 }
 
 export const testDriver = (tests: Test[]) => {
-  tests.forEach(({ name: name, input: programStr }: Test) => {
-    it(name, () => {
+  tests.forEach(({ name: name, input: programStr, output: expectedLogs, error: error }: Test) => {
+    it(name, async () => {
       const consoleErrorMock = jest.spyOn(console, 'error').mockImplementation()
+      const logs = new Set()
+      const consoleLogMock = jest.spyOn(console, 'log').mockImplementation(log => {
+        logs.add(log)
+      })
 
       const parsedCode = parseGoCode(programStr)
       expect(parsedCode).toBeTruthy()
@@ -19,10 +25,16 @@ export const testDriver = (tests: Test[]) => {
       expect(instrs.length).toBeGreaterThan(0)
 
       // TODO somehow check content of OS?
-      expect(() => run(instrs)).not.toThrow()
-      expect(consoleErrorMock).not.toHaveBeenCalled()
+      if (expectedLogs) {
+        await run(instrs)
+        expect(consoleErrorMock).not.toHaveBeenCalled()
+        expect(logs).toEqual(expectedLogs)
+      } else {
+        expect(() => compileGoCode(parsedCode)).toThrow(error)
+      }
       
       consoleErrorMock.mockRestore()
+      consoleLogMock.mockRestore()
     })
   })
 }
--- src/vm/__tests__/vm.test.ts
+++ src/vm/__tests__/vm.test.ts
@@ -9,7 +9,8 @@ const tests: Test[] = [
       func main() {
         fmt.Println(1);
       }
-    `
+    `,
+    output: new Set([1])
   },
   {
     name: 'Run sequential constructs: ShortVarDecl, arithmetic, function call correctly',
@@ -23,75 +24,105 @@ const tests: Test[] = [
       func main() {
         minus(42);
       }
-    `
+    `,
+    output: new Set([0])
   },
   {
     name: 'Run goroutines correctly',
     input: `
       package main
-      import "fmt"
+      import (
+        "fmt"
+        "sync"
+      )
+      var wg sync.WaitGroup
       func print(x int) {
         fmt.Println(x)
+        wg.Done()
       }
       func main() {
+        wg.Add(2)
         go print(1)
         go print(100 / 2)
+        wg.Wait()
       }
-    `
+    `,
+    output: new Set([1, 50])
   },
   {
     name: 'Run channels correctly',
     input: `
       package main
-      import "fmt"
+      import (
+        "fmt"
+        "sync"
+      )
+      var wg sync.WaitGroup
       func minus(c chan int) {
         x := <-c
         fmt.Println(x)
+        wg.Done()
       }
       func main() {
+        wg.Add(2)
         c := make(chan int)
         bc := make(chan int, 1)
         go minus(c)
         go minus(bc)
         c <- 21
         bc <- 212
+        wg.Wait()
       }
-    `
+    `,
+    output: new Set([21, 212])
   },
   {
     name: 'Run WaitGroup correctly',
     input: `
       package main
-      import "fmt"
-      func print(wg sync.WaitGroup) {
+      import (
+        "fmt"
+        "sync"
+      )
+      var wg sync.WaitGroup
+      func print() {
         fmt.Println(100)
         wg.Done()
       }
       func main() {
-        var wg sync.WaitGroup
         wg.Add(1)
         go print(wg)
         wg.Wait()
       }
-    `
+    `,
+    output: new Set([100])
   },
   {
     name: 'Run mutex correctly',
     input: `
       package main
-      import "fmt"
+      import (
+        "fmt"
+        "sync"
+      )
       var counter int = 0
       var mu sync.Mutex
+      var wg sync.WaitGroup
       func increment(x int) {
         mu.Lock();
         counter = counter + x;
         mu.Unlock();
+        wg.Done()
       }
       func main() {
+        wg.Add(2)
         go increment(10)
         go increment(2)
+        wg.Wait()
+        fmt.Println(counter)
       }
-    `
+    `,
+    output: new Set([12])
   }
 ]
 
